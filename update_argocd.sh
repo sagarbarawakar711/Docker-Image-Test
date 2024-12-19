bb8f44272fb525cb36a25e08a0b2b6e03e438198
sagarbarawakar711 <114502514+sagarbarawakar711@users.noreply.github.com>
2024-12-19 16:00:11 +0530

    Create update_argocd.sh

---
+++ update_argocd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="https://github.com/sagarbarawakar711/ArgoCD-Repo.git"
+IMAGE_TAG="461655781674.dkr.ecr.ap-south-1.amazonaws.com/argocd-repo:latest"
+
+# Clone the ArgoCD repository
+git clone $REPO_URL
+cd ArgoCD-Repo
+
+# Update the deployment.yaml file with the new image tag
+sed -i "s|image: .*|image: $IMAGE_TAG|" deployment.yaml
+
+# Configure Git
+git config --global user.email "sagarbarawakar1993@gmail.com"
+git config --global user.name "Sagar.Barawakar"
+
+# Commit and push the changes
+git add deployment.yaml
+git commit -m "Updated image to latest"
+git push origin main
